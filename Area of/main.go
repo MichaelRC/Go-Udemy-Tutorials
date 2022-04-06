7187bc4b86d79019bd63c65511e1125e348895c8
Michael Campitiello <michaelrcampi@gmail.com>
2022-04-06 10:43:23 -0700

    added getArea func

--- Area of/main.go
+++ Area of/main.go
@@ -4,9 +4,24 @@ type shape interface {
 	getArea() float64
 }
 
-type sqaure struct{}
-type triangle struct{}
+type sqaure struct{
+	sideLength int
+}
+type triangle struct{
+	height int
+	base int
+}
 
 func main() {
 
+}
+
+func (t triangle) getArea() float64 {
+	a := 0.5 * int float(t.base) * int float(t.height)
+	return float64(a) 
+}
+
+func (s sqaure) getArea() float64 {
+	a := s.sideLength * s.sideLength
+	return float64(a)
 }
