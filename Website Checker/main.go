c248ab12c4108f65fd8fed407011f4b488f7be95
Michael <michaelrcampi@gmail.com>
2022-04-10 15:39:03 -0700

    start of new project: website tracker

---
+++ Website Checker/main.go
@@ -0,0 +1,19 @@
+package main
+
+func main() {
+	links := []string{
+		//make sure to include web protocol!
+		//Go expects to see it and can use it to
+		//differentiate between http and https
+		"http://google.com",
+		"http://facebook.com",
+		"http://stackoverflow.com",
+		"http://golang.org",
+		"http://amazon.com",
+	}
+
+	for _, link := range links {
+
+	}
+
+}
